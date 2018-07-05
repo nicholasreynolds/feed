c80d631b0dd4f7618031c41b9e3a063300ab0f96
Nicholas <nicholas@localhost.localdomain>
2018-07-05 13:34:07 -0700

    Setup http req

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import angular from 'angular';
 
-import {hello} from './app/hello';
 import 'angular-ui-router';
 import routesConfig from './routes';
 
 import './index.less';
 
-export const app = 'app';
-
 angular
-  .module(app, ['ui.router'])
+  .module('app', ['ui.router'])
   .config(routesConfig)
-  .component('app', hello);
+  .controller('FeedController', function ($http, $log) {
+    const vm = this;
+    $http.get('/app/cities.list.json').then(res => {
+      vm.cities = res.data; // list of cities available for openweathermap api
+    });
+    vm.currCityIndex = 0; // the current index of json object in cities list
+    vm.apiKey = '9856613742166978ba4d7485e22d22b8';
+    vm.lastCall = 0;
+    vm.loadResults = function () {
+      const currTime = new Date().getSeconds();
+      if (currTime - vm.lastCall > 1) { // free openweather api only allows 1 call/second
+        $http.get('https://api.openweathermap.org/data/2.5/weather?id=' + vm.cities[vm.currCityIndex] + '&APPID=' + vm.apiKey).then(res => {
+          vm.lastCall = new Date().getSeconds();
+          $log.debug(res.data);
+        });
+      } else {
+        $log.debug('Cannot make more than 1 call/second');
+      }
+    };
+  });
