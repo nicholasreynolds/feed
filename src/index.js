7fe8a8fedf616cd8b0bc0f4dc2d81ef1a851065e
Nicholas <nicholas@localhost.localdomain>
2018-07-05 19:50:28 -0700

    Put rest calls into service

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 import angular from 'angular';
 
-import 'angular-ui-router';
-import routesConfig from './routes';
-
 import './index.less';
 
 angular
-  .module('app', ['ui.router'])
-  .config(routesConfig)
-  .controller('FeedController', function ($http, $log) {
+  .module('app', ['ngResource'])
+  .factory('appSharedService', ['$http', '$q', '$resource', function ($http, $q, $resource) {
+    return {
+      get(url) {
+        const deferred = $q.defer();
+        $http
+          .get(url)
+          .then(data => deferred.resolve(data), data => deferred.reject(data));
+        return deferred.promise;
+      },
+      res(uri) {
+        return $resource(uri);
+      }
+    };
+  }])
+  .controller('FeedController', ['$log', 'appSharedService', function ($log, appSharedService) {
     const vm = this;
-    $http.get('/app/cities.list.json').then(res => {
-      vm.cities = res.data; // list of cities available for openweathermap api
-    });
+    vm.cities = appSharedService.res('city.list.json').query(); // list of cities available for openweathermap api
     vm.currCityIndex = 0; // the current index of json object in cities list
     vm.apiKey = '9856613742166978ba4d7485e22d22b8';
     vm.lastCall = 0;
+    vm.entries = [];
     vm.loadResults = function () {
       const currTime = new Date().getSeconds();
-      if (currTime - vm.lastCall > 1) { // free openweather api only allows 1 call/second
-        $http.get('https://api.openweathermap.org/data/2.5/weather?id=' + vm.cities[vm.currCityIndex] + '&APPID=' + vm.apiKey).then(res => {
-          vm.lastCall = new Date().getSeconds();
-          $log.debug(res.data);
-        });
+      if (currTime - vm.lastCall > 1) { // free openweather api only allows 1 call/second.
+        appSharedService
+          .get('https://api.openweathermap.org/data/2.5/weather?id=' + vm.cities[vm.currCityIndex].id + '&APPID=' + vm.apiKey)
+          .then(data => {
+            vm.lastCall = new Date().getSeconds();
+            vm.entries.push(data); // entries list populates index.html table
+            vm.currCityIndex++;
+          });
       } else {
         $log.debug('Cannot make more than 1 call/second');
       }
     };
-  });
+  }]);
