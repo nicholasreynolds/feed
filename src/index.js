947e41f8daf46124c8a13a8dcfe2a1f149842037
Nicholas <nicholas@localhost.localdomain>
2018-07-06 12:28:51 -0700

    Add Usage details and reduce rest call time separation

--- src/index.js
+++ src/index.js
@@ -23,21 +23,15 @@ angular
     vm.cities = appSharedService.res('city.list.json').query(); // list of cities available for openweathermap api
     vm.currCityIndex = 0; // the current index of json object in cities list
     vm.apiKey = '9856613742166978ba4d7485e22d22b8';
-    vm.lastCall = 0;
     vm.entries = [];
     vm.loadResults = function () {
-      const currTime = new Date().getSeconds();
-      if (currTime - vm.lastCall > 1) { // free openweather api only allows 1 call/second.
-        appSharedService
-          .get('https://api.openweathermap.org/data/2.5/weather?id=' + vm.cities[vm.currCityIndex].id + '&APPID=' + vm.apiKey)
-          .then(data => {
-            vm.lastCall = new Date().getSeconds();
-            vm.entries.push(toActivity(data)); // entries list populates index.html table
-            vm.currCityIndex++;
-          });
-      } else {
-        $log.debug('Cannot make more than 1 call/second');
-      }
+      appSharedService
+        .get('https://api.openweathermap.org/data/2.5/weather?id=' + vm.cities[vm.currCityIndex].id + '&APPID=' + vm.apiKey)
+        .then(data => {
+          vm.lastCall = new Date().getSeconds();
+          vm.entries.push(toActivity(data)); // entries list populates index.html table
+          vm.currCityIndex++;
+        });
     };
   }])
   .directive('ngCount', ['$interval', function ($interval) {
